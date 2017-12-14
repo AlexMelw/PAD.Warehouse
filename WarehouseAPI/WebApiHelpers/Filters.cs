c5523f2abcd2c9c64e793597153529db91219f5c
dumitruguba <dumitru.guba@gmail.com>
2017-12-14 02:06:42 +0200

    Add filtering and pagination for Orders and Customers

--- WarehouseAPI/WebApiHelpers/Filters.cs
+++ WarehouseAPI/WebApiHelpers/Filters.cs
@@ -5,18 +5,37 @@ using System.Threading.Tasks;
 
 namespace WarehouseAPI.WebApiHelpers
 {
-    public class ProductFilter
+    public class Filter
+    {
+        public Paiging Page { get; set; }
+
+        public Filter()
+        {
+            Page = new Paiging();
+        }
+    }
+
+    public class ProductFilter : Filter
     {
         public string Label { get; set; }
         public decimal LPrice { get; set; } = 0;
         public decimal GPrice { get; set; } = decimal.MaxValue;
+    }
 
-        public Paiging Page { get; set; }
+    public class OrderFilter : Filter
+    {
+        public string Address { get; set; }
+        public string FName { get; set; }
+        public string LName { get; set; }
+    }
 
-        public ProductFilter()
-        {
-            Page = new Paiging();
-        }
-
+    public class CustomerFilter : Filter
+    {
+        public string FName { get; set; }
+        public string LName { get; set; }
+        public string FName_start_with { get; set; }
+        public string LName_start_with { get; set; }
+        public bool WithOrders { get; set; } = false;
+        public bool WithOrderDetails { get; set; } = false;
     }
 }
