f45ac60f8eac0e5a7bb3d787b193e6a7de36daa3
Dumbrava Alexandru <alexandru.dumbrava@ati.utm.md>
2017-12-20 17:33:10 +0200

    logging for CustomersController

--- WarehouseAPI/Controllers/CustomersController.cs
+++ WarehouseAPI/Controllers/CustomersController.cs
@@ -14,6 +14,7 @@ namespace WarehouseAPI.Controllers
     using Microsoft.AspNetCore.JsonPatch;
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.Logging;
     using Repositories.Context;
     using Repositories.Entities;
 
@@ -22,12 +23,15 @@ namespace WarehouseAPI.Controllers
     public class CustomersController : Controller
     {
         private readonly EShopContext _context;
+        private readonly ILogger<CustomersController> _logger;
+        private readonly EventId e;
 
         #region CONSTRUCTORS
 
-        public CustomersController(EShopContext context)
+        public CustomersController(EShopContext context, ILogger<CustomersController> logger)
         {
             _context = context;
+            _logger = logger;
         }
 
         #endregion
@@ -36,6 +40,8 @@ namespace WarehouseAPI.Controllers
         [HttpGet]
         public IActionResult GetCustomers([FromQuery] CustomerFilter filter)
         {
+            _logger.LogInformation("Retrieve all the customer.");
+     
             IQueryable<Customer> customers = _context.Customers;
 
             if (filter.LName != null)
@@ -66,7 +72,7 @@ namespace WarehouseAPI.Controllers
             }
 
             if (filter.WithOrders && filter.WithOrderDetails)
-            {
+            {         
                 return GetCustomersWithOrdersAndOrderDetails(customers);
             }
 
@@ -83,26 +89,33 @@ namespace WarehouseAPI.Controllers
         public async Task<IActionResult> GetCustomer([FromRoute] long id,
             bool withOrders = false, bool withOrderDetails = false)
         {
+            _logger.LogInformation($"Enter in {nameof(GetCustomer)} method.");
+
             if (!ModelState.IsValid)
             {
+                _logger.LogWarning(e, "Invalid model state.");
                 return BadRequest(ModelState);
             }
 
             if (!CustomerExists(id))
             {
+                _logger.LogCritical(e, $"User with ID={id} is not found.");
                 return NotFound();
             }
 
             if (withOrders && withOrderDetails)
             {
+                _logger.LogInformation($"Request for both: orders and orders details of customer ID={id}.");
                 return await GetCustomerWithOrdersAndOrderDetails(id);
             }
 
             if (withOrders && !withOrderDetails)
             {
+                _logger.LogInformation($"Request for orders only of customer ID={id}.");
                 return await GetCustomerWithOrders(id);
             }
 
+            _logger.LogInformation($"Leaving the {nameof(GetCustomer)} method.");
             return await GetCustomerOnly(id);
         }
 
@@ -110,8 +123,10 @@ namespace WarehouseAPI.Controllers
         [HttpPut("{id}")]
         public async Task<IActionResult> PutCustomer([FromRoute] long id, [FromBody] CustomerToUpdateDTO customerDTO)
         {
+            _logger.LogInformation($"Enter in {nameof(PutCustomer)} method.");
             if (!ModelState.IsValid)
             {
+                _logger.LogWarning(e, "Invalid model state.");
                 return BadRequest(ModelState);
             }
 
@@ -121,17 +136,20 @@ namespace WarehouseAPI.Controllers
 
             try
             {
+                _logger.LogInformation($"Salvam in BD customer cu ID={id}.");
                 await _context.SaveChangesAsync();
             }
             catch (DbUpdateConcurrencyException)
             {
                 if (!CustomerExists(id))
                 {
+                    _logger.LogCritical(e, $"Concurrency violation while saving customer with ID={id}.");
                     return NotFound();
                 }
                 return StatusCode(500, $"Simultaneously attempt to modify {nameof(Customer)} entity.");
             }
 
+            _logger.LogInformation($"Leaving the {nameof(PutCustomer)} method.");
             return NoContent();
         }
 
@@ -156,20 +174,24 @@ namespace WarehouseAPI.Controllers
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteCustomer([FromRoute] long id)
         {
+            _logger.LogInformation($"Enter in {nameof(DeleteCustomer)} method.");
             if (!ModelState.IsValid)
             {
+                _logger.LogWarning(e, "Invalid model state.");
                 return BadRequest(ModelState);
             }
 
             var customer = await _context.Customers.SingleOrDefaultAsync(m => m.Id == id);
             if (customer == null)
             {
+                _logger.LogCritical(e, $"User with ID={id} is not found.");
                 return NotFound();
             }
-
+            _logger.LogInformation($"Remove customer with ID={id} and save changes.");
             _context.Customers.Remove(customer);
             await _context.SaveChangesAsync();
 
+            _logger.LogInformation($"Leaving the {nameof(DeleteCustomer)} method.");
             return Ok(customer);
         }
 
@@ -178,13 +200,16 @@ namespace WarehouseAPI.Controllers
         public async Task<IActionResult> PatchCustomer(long id,
             [FromBody] JsonPatchDocument<CustomerToPatchDTO> patchDoc)
         {
+            _logger.LogInformation($"Enter in {nameof(PatchCustomer)} method.");
             if (patchDoc == null)
             {
+                _logger.LogWarning("Patch document isn't provided or is invalid.");
                 return BadRequest();
             }
 
             if (!CustomerExists(id))
             {
+                _logger.LogCritical(e, $"User with ID={id} is not found.");
                 return NotFound();
             }
 
@@ -198,11 +223,13 @@ namespace WarehouseAPI.Controllers
 
             if (!ModelState.IsValid)
             {
+                _logger.LogWarning(e, "Invalid model state.");
                 return BadRequest(ModelState);
             }
 
             if (patchedCustomer.Id != customer.Id)
             {
+                _logger.LogWarning("An attempt of modifying entity's ID was occurred.");
                 ModelState.AddModelError(nameof(customer.Id), "Modification of ID isn't allowed.");
                 return BadRequest(ModelState);
             }
@@ -211,9 +238,10 @@ namespace WarehouseAPI.Controllers
 
             if (_context.SaveChanges() == 0)
             {
+                _logger.LogError("No entities were saved.");
                 return StatusCode(500, "A problem happened while handling your request.");
             }
-
+            _logger.LogInformation("Successfully saved entety's modifications.");
             return NoContent();
         }
 
